66b0a03c1a5c604222d4a296bdb74a20ffe220f8
santhosh H <santhosh_201032@yahoo.in>
2021-06-23 14:36:03 +0530

    Update fuzz.py

--- fuzzywuzzy/fuzz.py
+++ fuzzywuzzy/fuzz.py
@@ -25,7 +25,7 @@ def ratio(s1, s2):
     s1, s2 = utils.make_type_consistent(s1, s2)
 
     m = SequenceMatcher(None, s1, s2)
-    return utils.intr(100 * m.ratio())
+    return 100 * m.ratio()
 
 
 @utils.check_for_none
